6976c0bfe97cb55be8b0f311736ea52c0e534c1f
KnoVz <h@knovz.com>
2026-02-22 18:09:05 +0100

    routes with variables

--- app.py
+++ app.py
@@ -24,5 +24,41 @@ def hello_world():
     return f"<p>Hello, {escape(name)}!</p>"
 
 
+# Routes with vars
+@app.route("/user/<username>")
+def show_user_profile(username):
+    """route with variable"""
+    return f"User: {escape(username)}"
+
+
+# Id the param is not an int, it will not use this route.
+# If there is another route /post/str: it will use the mathching one
+# depending on the post_id type
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    """route with converted variable"""
+    return f"Post {post_id}"
+
+
+@app.route("/path/<path:subpath>")
+def show_subpath(subpath):
+    """route with path conversion"""
+    return f"Subpath {escape(subpath)}"
+
+
+# Type conversions
+# string | (default) accepts any text without a slash
+# int    | accepts positive integers
+# float  | accepts positive floating point values
+# path   | like string but also accepts slashes
+# uuid   | accepts UUID strings
+
+# Trainling slashes
+# /projects/
+# /about
+# If the user types /projects it will be redirected to /projects/
+# If the user types /about/ it will be "not found"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
