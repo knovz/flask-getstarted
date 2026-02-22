57b44b9d619e55f66ebcc38e68cfc47b6fdfc49d
KnoVz <h@knovz.com>
2026-02-22 17:28:07 +0100

    Always escape user input

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask import request
+from markupsafe import (
+    escape,
+)  # Escape all user input. Jinja templates do it automatically
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    name = request.args.get("name", "Flask")
+    return f"<p>Hello, {escape(name)}!</p>"
 
 
 if __name__ == "__main__":
